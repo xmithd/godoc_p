c37d8dc5fe99786a8e325398ef6b24d7f8978f00
xmithd <fireficus@gmail.com>
2016-12-15 19:14:12 +0800

    uri was missing a slash

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const uri = `${process.env.PUBLIC_URL}data/`;
+const uri = `${process.env.PUBLIC_URL}/data/`;
 
 // For the prototype: get the json data
 export const searchNearbyDoctors = () => axios.get(`${uri}nearby_doctors.json`);
